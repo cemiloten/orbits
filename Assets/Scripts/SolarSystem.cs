fb65ac86440a8eb25b5e26815368ce923c73a446
Cemil Oten <cemil.oten@gmail.com>
2019-02-28 17:58:49 +0100

    first commit

---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    public GameObject sunPrefab;
+    public GameObject planetPrefab;
+    public Vector3 center;
+
+    private GameObject sun;
+    private GameObject planet;
+    private float angle = 0f;
+
+    private void Start()
+    {
+        sun = Instantiate(sunPrefab, Vector3.zero, Quaternion.identity);
+        planet = Instantiate(planetPrefab, new Vector3(2f, 2f, 0f), Quaternion.identity);
+    }
+
+    private void Update()
+    {
+        angle += Time.deltaTime;
+        float range = 2f;
+        planet.transform.position = new Vector3(range * Mathf.Cos(angle), range * Mathf.Sin(angle), 0f);
+    }
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TouchManager
+{
+    public struct TouchInfo
+    {
+        public int fingerId;
+        public Vector2 startPosition;
+        public Vector2 position;
+    }
+
+    public delegate void OnTouchCountChangedHandler();
+    public static event OnTouchCountChangedHandler OnTouchCountChanged;
+
+    public delegate void OnTouchStartHandler(int fingerdID, Vector2 startPosition);
+    public static event OnTouchStartHandler OnTouchStart;
+
+    public delegate void OnTouchHoldHandler(int fingerdID, Vector2 newPosition);
+    public static event OnTouchHoldHandler OnTouchHold;
+
+    public delegate void OnTouchMoveHandler(int fingerdID, Vector2 newPosition);
+    public static event OnTouchMoveHandler OnTouchMove;
+
+    public delegate void OnTouchEndHandler(int fingerdID, Vector2 endPosition);
+    public static event OnTouchEndHandler OnTouchEnd;
+
+    public delegate void OnTouchCancelHandler(int fingerdID, Vector2 endPosition);
+    public static event OnTouchCancelHandler OnTouchCancel;
+
+    public static int TouchCount { get; private set; }
+
+    public static void Update()
+    {
+        if (Input.touchCount != TouchCount && OnTouchCountChanged != null)
+        {
+            TouchCount = Input.touchCount;
+            OnTouchCountChanged();
+        }
+
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; ++i)
+            {
+                Touch touch = Input.touches[i];
+                if (touch.phase == TouchPhase.Began && OnTouchStart != null)
+                {
+                    OnTouchStart(touch.fingerId, touch.position);
+                }
+                else if (touch.phase == TouchPhase.Stationary && OnTouchHold != null)
+                {
+                    OnTouchHold(touch.fingerId, touch.position);
+                }
+                else if (touch.phase == TouchPhase.Moved && OnTouchMove != null)
+                {
+                    OnTouchMove(touch.fingerId, touch.position);
+                }
+                else if (touch.phase == TouchPhase.Ended && OnTouchEnd != null)
+                {
+                    OnTouchEnd(touch.fingerId, touch.position);
+                }
+                else if (touch.phase == TouchPhase.Canceled && OnTouchCancel != null)
+                {
+                    OnTouchCancel(touch.fingerId, touch.position);
+                }
+            }
+        }
+    }
+}
